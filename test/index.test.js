a00e9a45e4eb00ff1fa26c03e2b4b95912b677f0
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2016-12-17 15:03:58 +0100

    Install dependencies for write tests

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+var animals = require ('../index');
+
+describe('Hello world testing !', function() {
+  it('should be true', function() {
+    expect(true).to.be.true;
+  })
+});
