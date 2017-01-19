270de48be047eee3e5871719d1ba9d06fd592bf2
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-01-19 18:34:20 +0100

    Use 3 assertions Assert / Expect / Should to pass the same test

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,6 @@
+var assert = require('chai').assert;
 var expect = require('chai').expect;
+var should = require('chai').should();
 
 var animals = require ('../index').animals;
 
@@ -13,4 +15,24 @@ describe('Animals list', function() {
     var famousNames = animals.map(animal => animal.name);
     expect(famousNames).to.include('Grumpy');
   })
+});
+
+describe('same test with Assert / Expect / Should', function() {
+
+  describe('GIVEN a list of 4 colours: white, blue, green, red', function() {
+    var colours = ['white', 'blue', 'green', 'red'];
+    // Assert
+    it('WHEN the client request the list THEN 4 items are returned', function() {
+      assert.lengthOf(colours, 4);
+    })
+    // Expect
+    it('WHEN the client request the list THEN 4 items are returned', function() {
+      expect(colours).to.have.lengthOf(4);
+    })
+    // Should
+    it('WHEN the client request the list THEN 4 items are returned', function() {
+      colours.should.have.lengthOf(4);
+    })
+
+  });
 });
