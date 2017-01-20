14598f6f2ad471fe2e2d27fb29f9473b920239d9
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-01-20 16:00:36 +0100

    Workshop basket : basket with 2 same product, introduce getUnique function
    Workshop basket : basket with 8 items with 3 same product (test only)

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 
+let getUnique = (array) => {
+  let newArray = [];
+  let checker = {};
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    if(!checker[item]) {
+      checker[item] = true;
+      newArray.push(item);
+    }
+  }
+
+  return newArray;
+}
+
 let summarizeBasket = (prices, products) => {
 
   let price = 0;
-  let countProducts = 0;
-  let countArticles = 0;
 
   for (let i = 0; i < products.length; i++) {
     let product = products[i];
-    countArticles++;
-    countProducts++;
     price += prices[product];
   }
 
   return {
     price: price,
-    countArticles: countArticles,
-    countProducts: countProducts
+    countArticles: products.length,
+    countProducts: getUnique(products).length
   }
 }
 
 module.exports = {
-  summarizeBasket: summarizeBasket
+  summarizeBasket: summarizeBasket,
+  getUnique: getUnique
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 
 var summarizeBasket = require ('../index').summarizeBasket;
+var getUnique = require ('../index').getUnique;
 
 let prices = {
   banana: 1,
@@ -34,6 +35,44 @@ describe('summarizeBasket', function() {
     expect(result.price).to.be.equal(8);
     expect(result.countArticles).to.be.equal(2);
     expect(result.countProducts).to.be.equal(2);
+  });
+
+  it('Basket with 2 same product should return basic sum of prices but with one countProducts', function() {
+    var result = summarizeBasket(prices, ['salad', 'salad']);
+    expect(result.price).to.be.equal(10);
+    expect(result.countArticles).to.be.equal(2);
+    expect(result.countProducts).to.be.equal(1);
+  });
+
+  it('Basket with 8 items with 3 same product in should return basic sum of prices but with 5 countProducts', function() {
+    var result = summarizeBasket(prices, ['potato', 'banana', 'potato', 'banana', 'cucumber', 'salad', 'apple', 'cucumber']);
+    expect(result.price).to.be.equal(25);
+    expect(result.countArticles).to.be.equal(8);
+    expect(result.countProducts).to.be.equal(5);
+  });
+
+});
+
+describe('function to return array with distinct values', function() {
+
+  it('Empty array should return empty array', function() {
+    var result = getUnique([]);
+    expect(result).to.be.empty;
+  });
+
+  it('Array with one value should return array with same value', function() {
+    var result = getUnique(['tutu']);
+    expect(result).to.be.eql(['tutu']);
+  });
+
+  it('Array with 2 differents values should return array with same values', function() {
+    var result = getUnique(['titi', 'toto']);
+    expect(result).to.be.eql(['titi', 'toto']);
+  });
+
+  it('Array with 3 items with 2 same values should return array with 2 different values', function() {
+    var result = getUnique(['tata', 'tete', 'tata']);
+    expect(result).to.be.eql(['tata', 'tete']);
   });
 
 });
