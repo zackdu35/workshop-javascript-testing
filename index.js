0d386cb6ac61672f73995d185ebfca8885073b3a
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-01-20 14:51:00 +0100

    Workshop basket : refactoring with for loop

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 
 let summarizeBasket = (prices, products) => {
-  if(!products || !products.length) {
-    return {
-      price: 0,
-      countArticles: 0,
-      countProducts: 0
-    };
-  } else if (products.length === 1) {
-    let product = products[0];
-    let price = prices[product];
-    return {
-      price: price,
-      countArticles: 1,
-      countProducts: 1
-    };
-  } else if (products.length === 2) {
-    let product1 = products[0];
-    let product2 = products[1];
-    let price = prices[product1] + prices[product2];
-    return {
-      price: price,
-      countArticles: 2,
-      countProducts: 2
-    };
-  } else {
-    return 'TO BE CONTINUED';
+
+  let price = 0;
+  let countProducts = 0;
+  let countArticles = 0;
+
+  for (let i = 0; i < products.length; i++) {
+    let product = products[i];
+    countArticles++;
+    countProducts++;
+    price += prices[product];
+  }
+
+  return {
+    price: price,
+    countArticles: countArticles,
+    countProducts: countProducts
   }
 }
 
